508a5267e098be0ed222081f656ebe530e1a90b7
KLU <md.aman88975@gmail.com>
2026-06-03 11:14:12 +0530

    initialcommit

---
+++ WEEK/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Login from './component/Login';
+import './App.css';
+function App() {
+    return (
+        <div>
+            <Login />
+        </div>
+    );
+}
+export default App;
